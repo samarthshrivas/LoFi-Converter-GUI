00330030cd3a48f2242a8b3511f9f530ef4e1e35
SamarthShrivas <SamarthShrivas@users.noreply.github.com>
2023-08-09 14:02:20 +0530

    Update web.py
    Limiting videos to download which are less than 10 minutes

--- web.py
+++ web.py
@@ -12,22 +12,46 @@ def delete_temp_files(audio_file, output_file, mp3_file):
     if mp3_file:
         os.remove(mp3_file)
 
+
+@st.cache_data(ttl=600, show_spinner=False)
+def isDownlaodable(youtube_link):
+    try:
+        with yt_dlp.YoutubeDL({'format': 'bestaudio', "quiet":True}) as ydl:
+            dur = None
+            info_dict = ydl.extract_info(youtube_link, download=False)
+            for i in info_dict['formats']:
+                if "duration" in i['fragments'][0].keys():
+                    dur = i['fragments'][0]["duration"]
+                    break
+
+    except Exception as e:
+        st.error("Make sure Youtube video is publicly globally avilable!!!")
+        print(f"ERROR: {e} ==> {youtube_link}")
+        return False
+    if dur <= 600:
+        return True
+    else:
+        st.error("Make sure song less than 10 minutes")
+        return False
+
 # Function to download YouTube audio and save as a WAV file
 @st.cache_data(ttl=600)
 def download_youtube_audio(youtube_link):
     uu = str(uuid.uuid4())
-    try:
-        with yt_dlp.YoutubeDL({'format': 'bestaudio/best', 'outtmpl': 'uploaded_files/' + uu + '.%(ext)s', "quiet":True}) as ydl:
-            info_dict = ydl.extract_info(youtube_link, download=True)
-            audio_file = ydl.prepare_filename(info_dict)
-            song_name = info_dict['title']
-        print(f"Downloaded YouTube link: {youtube_link} ==> {song_name}")
-        mp3_file_base = music.msc_to_mp3_inf(audio_file)
-        return (audio_file, mp3_file_base, song_name)
-    except Exception as e:
-        st.error("Make sure Youtube video is publicly globally avilable!!!")
-        print(f"ERROR: {e} ==> {youtube_link}")
-        return None
+
+    if isDownlaodable(youtube_link):
+        try:
+            with yt_dlp.YoutubeDL({'format': 'bestaudio/best', 'outtmpl': 'uploaded_files/' + uu + '.%(ext)s', "quiet":True}) as ydl:
+                info_dict = ydl.extract_info(youtube_link, download=True)
+                audio_file = ydl.prepare_filename(info_dict)
+                song_name = info_dict['title']
+            print(f"Downloaded YouTube link: {youtube_link} ==> {song_name}")
+            mp3_file_base = music.msc_to_mp3_inf(audio_file)
+            return (audio_file, mp3_file_base, song_name)
+        except Exception as e:
+            # st.error("Error")
+            # print(f"ERROR: {e} ==> {youtube_link} in download_youtube_audio")
+            return None
 
 # Main function for the web app
 def main():
