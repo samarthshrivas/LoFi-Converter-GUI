2871d39398b21d5840350c6eedbaa6ec3d23ab24
SamarthShrivas <SamarthShrivas@users.noreply.github.com>
2024-03-03 01:36:49 +0530

    Update web.py

--- web.py
+++ web.py
@@ -60,7 +60,7 @@ def main():
     st.set_page_config(page_title="Lofi Converter", page_icon=":microphone:", layout="wide", )
     
     st.title(":microphone: Lofi Converter")
-    st.markdown("# Bookmark new [Website](https://lofi-convertor.samevibe.in/)")
+    st.markdown("# Bookmark new [Website](https://lofi-converter.samevibe.in/)")
     st.info("Tip: Use Headphone for best experience :headphones:")
     youtube_link = st.text_input("Enter the YouTube link 🔗 of the song to convert:", placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     try:
